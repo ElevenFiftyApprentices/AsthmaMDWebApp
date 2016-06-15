f6153a230359eb76d250273cf38171a008aeb60e
Scott Kelly <sctkelly@gmail.com>
2016-06-15 10:52:00 -0400

    Added ChildEntity class.

---
+++ AsthmaMDWebApp.Web/AsthmaMDWebApp.Data/ChildEntity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AsthmaMDWebApp.Data
+{
+    public class ChildEntity
+    {
+        [Key]
+        public int ChildId { get; set; }
+
+        [Display(Name ="Child Name")]
+        public string ChildName { get; set; }
+
+        [Display(Name ="Child Age")]
+        public int ChildAge { get; set; }
+
+        [Display(Name ="Child Height")]
+        public float ChildHeight { get; set; }
+
+        [Display(Name ="Peak Flow Meter")]
+        public int PeakFlowMeter { get; set; }
+
+        [Display(Name ="FEV-1")]
+        public float FEV1 { get; set; }
+
+        public GenderType Gender { get; set; }
+
+        public enum GenderType
+        {
+            Male = 1,
+            Female = 2,
+            Other = 3
+        }
+    }
+}
