b2de58bf2ec7306fcc1755b6de87f0beb2e525c5
NickLawson <nicklawson760@gmail.com>
2016-06-15 17:16:02 -0400

    Added UserProfile variables and saving to DB, and fixed webconfig

--- AsthmaMDWebApp.Web/AsthmaMDWebApp.Data/UserProfile.cs
+++ AsthmaMDWebApp.Web/AsthmaMDWebApp.Data/UserProfile.cs
@@ -1,11 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AsthmaMDWebApp.Data
 {
+    //UserProfile stores all the values for User Accounts
     public class UserProfile
     {
+        public string UserProfileId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleInitial { get; set; }
+
+        public string LastName { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+        public string Address1 { get; set; }
+
+        public string Address2 { get; set; }
+
+        public uint ZipCode { get; set; }
+
+        public Dictionary<string, int> ChildIds { get; set; }
     }
 }
--- AsthmaMDWebApp.Web/AsthmaMDWebApp.Web/Controllers/AccountController.cs
+++ AsthmaMDWebApp.Web/AsthmaMDWebApp.Web/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using AsthmaMDWebApp.Web.Models;
+using AsthmaMDWebApp.Data;
 
 namespace AsthmaMDWebApp.Web.Controllers
 {
@@ -153,17 +154,33 @@ namespace AsthmaMDWebApp.Web.Controllers
             {
                 var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
+
+
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
-                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    //Adds extra properties to AspnetUser
+                    UserProfile UserProfile = new UserProfile();
+
+                    using (var ctx = new AsthmaDbContext())
+                    {
+                        //Creates the properties
+                        UserProfile.UserProfileId = user.Id;
+                        UserProfile.FirstName = model.FirstName;
+                        UserProfile.MiddleInitial = model.MiddleInitial;
+                        UserProfile.LastName = model.LastName;
+                        UserProfile.PhoneNumber = model.PhoneNumber;
+                        UserProfile.Address1 = model.Address1;
+                        UserProfile.Address2 = model.Address2;
+                        UserProfile.ZipCode = model.ZipCode;
+
+                        //Saves to the DB
+                        ctx.UserProfiles.Add(UserProfile);
+                        ctx.SaveChanges();
+                    }
+
+                        return RedirectToAction("Index", "Home");
                 }
                 AddErrors(result);
             }
