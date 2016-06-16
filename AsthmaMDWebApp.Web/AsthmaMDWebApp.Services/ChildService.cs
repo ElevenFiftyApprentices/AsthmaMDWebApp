26fb6b7551c67c6d22ee5a89b66c72866f43a0e9
Scott Kelly <sctkelly@gmail.com>
2016-06-16 08:49:05 -0400

    Working on child controller and service but I have to restart computer.

---
+++ AsthmaMDWebApp.Web/AsthmaMDWebApp.Services/ChildService.cs
@@ -0,0 +1,41 @@
+﻿using AsthmaMDWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AsthmaMDWebApp.Services
+{
+    public class ChildService
+    {
+
+        private readonly Guid _userId;
+
+        public ChildService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public IEnumerable<ChildViewModel> GetChildren()
+        {
+            using (var ctx = new AsthmaDbcontext())
+            {
+                var kids = ctx
+                        .Children
+                        .Where(e => e.UserId == _userId);
+                return kids
+                    .ctx
+                    .Select(e => new ChildViewModel
+                    {
+                        ChildId = e.ChildId,
+                        ChildName = e.ChildName,
+                        ChildAge = e.ChildAge,
+                        ChildHeight = e.ChildHeight,
+                        ChildPeakFlowMeter = e.ChildPeakFlowMeter,
+                        ChildFEV1 = e.ChildFEV1
+                    })
+                    .ToList();
+            }
+        }
+    }
+}
