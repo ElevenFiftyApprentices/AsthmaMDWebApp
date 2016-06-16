642e024c447d5ddedfd0c036a1d2445d75bdff18
NickLawson <nicklawson760@gmail.com>
2016-06-16 10:48:44 -0400

    Accessing UserProfile

---
+++ AsthmaMDWebApp.Web/AsthmaMDWebApp.Web/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using AsthmaMDWebApp.Data;
+using AsthmaMDWebApp.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AsthmaMDWebApp.Web.Controllers
+{
+    public class UserController
+    {
+        private readonly Lazy<UserService> _svc;
+
+        public UserController()
+        {
+            _svc =
+                new Lazy<UserService>(
+                    () =>
+                    {
+                        User.Identity.GetUserId();
+                        return new UserService(userId);
+                    });
+        }
+    }
+}
